70a68a05857ba34352462d92e244a3b032a5bbb6
Shaurya Yadav <shaurya.y321@gmail.com>
2022-10-22 15:37:19 +0530

    added few more lines
    introducing variables in python

--- python.py
+++ python.py
@@ -1,1 +1,7 @@
 print("Hello World")
+# variables in python
+# variables are simply storage where you store data (numbers, strings) on which your code's whole logic is based on.
+# example) Down below, I have created 2 variables which stores some data in the form of numbers & I'm using this data to reach the conclusion of my next line of code. 
+num1=1 #variable 1
+num2=2 #variable 2
+print(num1+num2)
